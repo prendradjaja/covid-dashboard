1d785fec68db5f666159603766f0b035fe41421a
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-04-01 22:48:26 -0700

    URL parser
    commit d4037e20f82dab49db47f88401af8cbc6bc087e8
    Author: Paul Kang <paulkang@amazon.com>
    Date:   Tue Mar 31 22:32:15 2020 -0700
    
        Adding preliminary url-parse logic (working!!)

--- src/app/multi-line-chart/multi-line-chart.component.ts
+++ src/app/multi-line-chart/multi-line-chart.component.ts
@@ -22,6 +22,7 @@ export class MultiLineChartComponent implements OnInit {
   // - all the serieses are of the same length & the same dates
   @Input() data: Series[];
   @Input() animate: boolean = false;
+  @Input() locations: string[];
   @Input() xAxisBounds?: [number, number];
   @Input() yAxisBounds?: [number, number];
 
