c3e7b0ef1fdaa3fc3a9c938d34f9849a80bc0c56
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-04-16 16:13:42 -0700

    Add note about ?animate bug

--- src/lib/URLState/types.ts
+++ src/lib/URLState/types.ts
@@ -4,6 +4,6 @@ export type CovidGraphDefinition = {
   data_type: string;
   x_axis_bounds: number[];
   y_axis_bounds: number[];
-  animate: boolean;
+  animate: boolean; // Bug: if animate=false, it's treated as true. Must omit in order to use false
   // Add more as needed
 };
