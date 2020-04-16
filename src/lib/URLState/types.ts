fbe1fa6330b0ff31a5df98989d0d7fdc1660af78
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-04-16 11:40:17 -0700

    Add axis bounds as URL params

--- src/lib/URLState/types.ts
+++ src/lib/URLState/types.ts
@@ -2,5 +2,7 @@ export type CovidGraphDefinition = {
   locations: string[];
   cutoff: number;
   data_type: string;
+  x_axis_bounds: number[];
+  y_axis_bounds: number[];
   // Add more as needed
 };
